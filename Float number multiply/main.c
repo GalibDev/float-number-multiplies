58249575bef95465ecd3d304b3a81a1548d2996a
GalibDev <mirza.galib.palash@gmail.com>
2025-03-03 23:29:30 +0600

    Add files via upload

---
+++ Float number multiply/main.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+
+{
+ float A1,A2,sum;
+ printf("Enter first number---");
+
+  scanf("%f",&A1);
+  printf("Enter the second number-----");
+
+  scanf("%f",&A2);
+
+sum=A1*A2;
+
+
+  printf("multiples of A1 and A2=%f\n",sum);
+
+
+
+
+
+
+
+
+
+}
